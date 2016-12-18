a42eabe2652a1b95766f2a69d82c7db4ed8083e3
arookas <arookas@outlook.com>
2016-12-18 11:48:19 -0500

    Store reference type/path information
    This will allow BLO elements to later save this information to a
    different format.

--- blojob/resource.cs
+++ blojob/resource.cs
@@ -8,8 +8,45 @@ namespace arookas {
 
 	abstract class bloResource {
 
+		protected bloResourceType mResourceType;
+		protected string mResourcePath, mResourceAbsPath;
+
 		public abstract void load(Stream stream);
 
+		public bloResourceType getResourceType() {
+			return mResourceType;
+		}
+		public string getResourcePath() {
+			return mResourcePath;
+		}
+		public string getResourceAbsPath() {
+			return mResourceAbsPath;
+		}
+
+		public bloResourceType setResourceType(bloResourceType type) {
+			bloResourceType old = mResourceType;
+			mResourceType = type;
+			return old;
+		}
+		public string setResourcePath(string path) {
+			string old = mResourcePath;
+			mResourcePath = path;
+			return old;
+		}
+		public string setResourceAbsPath(string path) {
+			string old = mResourceAbsPath;
+			mResourceAbsPath = path;
+			return old;
+		}
+
+	}
+	
+	enum bloResourceType {
+		None, // null reference
+		Unknown, // not used by SMS
+		LocalDirectory, // resource is in the specified directory of the specified archive
+		LocalArchive, // resource is in specified archive
+		Global, // resource may be found in any loaded global resource
 	}
 
 	class bloResourceFinder {
@@ -39,21 +76,21 @@ namespace arookas {
 			where T : bloResource, new() {
 
 			long start = reader.Position;
-			ReferenceType type = (ReferenceType)reader.Read8();
+			bloResourceType type = (bloResourceType)reader.Read8();
 			int length = reader.Read8();
 			string name = reader.ReadString(length);
 
 			string path = null;
 			switch (type) {
-				case ReferenceType.LocalDirectory: {
+				case bloResourceType.LocalDirectory: {
 					path = Path.Combine(mLocalPath, directory, name);
 					break;
 				}
-				case ReferenceType.LocalArchive: {
+				case bloResourceType.LocalArchive: {
 					path = Path.Combine(mLocalPath, name);
 					break;
 				}
-				case ReferenceType.Global: {
+				case bloResourceType.Global: {
 					foreach (string globalPath in mGlobalPaths) {
 						path = Path.Combine(globalPath, name);
 						if (!File.Exists(path)) {
@@ -68,22 +105,17 @@ namespace arookas {
 			T resource = null;
 			if (path != null && File.Exists(path)) {
 				resource = new T();
+				resource.setResourceType(type);
+				resource.setResourcePath(name);
+				resource.setResourceAbsPath(path);
 				using (Stream stream = File.OpenRead(path)) {
 					resource.load(stream);
 				}
 			}
-			if (resource == null && type != ReferenceType.None) {
+			if (resource == null && type != bloResourceType.None) {
 				Console.WriteLine(">>> FAILED: could not find {0} resource '{1}' at 0x{2:X}.", type, name, start);
 			}
 			return resource;
-		}
-
-		enum ReferenceType {
-			None, // null reference
-			Unknown, // not used by SMS
-			LocalDirectory, // resource is in the specified directory of the specified archive
-			LocalArchive, // resource is in specified archive
-			Global, // resource may be found in any loaded global resource
 		}
 
 	}
